6483a157c2462025ab4f603df7c88b06ea1b759a
YOCB <31845916+YOCB@users.noreply.github.com>
2018-04-23 22:14:14 +0300

    Update evaluate_results.m

--- evaluate_results.m
+++ evaluate_results.m
@@ -5,7 +5,7 @@
 input_dir = fullfile(pwd,'your_results');
 
 % Directory with ground truth images
-GD_dir = fullfile(pwd,'self_validation_HR');
+GT_dir = fullfile(pwd,'self_validation_HR');
 
 % Number of pixels to shave off image borders when calcualting scores
 shave_width = 4;
@@ -15,7 +15,7 @@ verbose = true;
 
 %% Calculate scores and save
 addpath utils
-scores = calc_scores(input_dir,GD_dir,shave_width,verbose);
+scores = calc_scores(input_dir,GT_dir,shave_width,verbose);
 
 % Saving
 save('your_scores.mat','scores');
