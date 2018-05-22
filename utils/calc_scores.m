d1d7dd0b57bf270874aaf8c5e12c05e80bc34e50
YOCB <31845916+YOCB@users.noreply.github.com>
2018-05-22 22:33:07 +0300

    Update calc_scores.m

--- utils/calc_scores.m
+++ utils/calc_scores.m
@@ -2,6 +2,13 @@ function scores = calc_scores(input_dir,GT_dir,shave_width,verbose)
 
 addpath(genpath(fullfile(pwd,'utils')));
 
+%% Loading model
+load modelparameters.mat
+blocksizerow    = 96;
+blocksizecol    = 96;
+blockrowoverlap = 0;
+blockcoloverlap = 0;
+
 %% Reading file list
 file_list = dir([input_dir,'/*.png']);
 im_num = length(file_list);
@@ -24,6 +31,8 @@ for ii=1:im_num
     scores(ii).name = file_list(ii).name;
     scores(ii).MSE = immse(input_image,GD_image);
     scores(ii).Ma = quality_predict(input_image);
+    scores(ii).NIQE = computequality(input_image,blocksizerow,blocksizecol,...
+        blockrowoverlap,blockcoloverlap,mu_prisparam,cov_prisparam);
 end
 
 end
